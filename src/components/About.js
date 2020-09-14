91ba3311874a43536cec34439820bb0e04caaa96
Juan <juan.rigual>
2020-09-14 13:07:42 -0300

    Added validations for mail and name input

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Container, Box, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+}));
 
 export default function About() {
+    const classes = useStyles();
+
     return (
-        <>
-            About
-        </>
+        <Container className={classes.root}>
+            <Box>
+                ABOUT
+            </Box>
+        </Container>
     )
 }
